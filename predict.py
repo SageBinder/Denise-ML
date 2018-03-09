856ccde6590f4dc60eef568a61716b3eed08b806
SageBinder <sagebin13@gmail.com>
2018-03-09 16:57:05 -0700

    train.py saves model, predict.py loads and continues training

--- predict.py
+++ predict.py
@@ -2,20 +2,40 @@ import tensorflow as tf
 import os
 import numpy as np
 import scipy.misc
+import random
+from scripts import ml_functions as ml
 
-image_directory = os.fsencode("out\\greyscale_200x200_labeled")
+x_total, y_total = ml.parse_full_data_greyscale('resources/training_mat_data/greyscale_200x200.mat')
 
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('.\\models\\test_save-0.meta')
-    saver.restore(sess, tf.train.latest_checkpoint('.\\models'))
+c = list(zip(x_total, y_total))
+random.shuffle(c)
+x_total, y_total = zip(*c)
+
+x_train = x_total[0:350]
+y_train = y_total[0:350]
+
+image_directory = os.fsencode("out/greyscale_200x200_labeled")
+export_dir = 'models/test_save'
+
+with tf.Session(graph=tf.Graph()) as sess:
+    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.TRAINING], export_dir)
     graph = tf.get_default_graph()
 
-    Z = graph.get_tensor_by_name(name="Z:0")
+    Y = graph.get_tensor_by_name(name="Y:0")
     X = graph.get_tensor_by_name(name="X:0")
+    Z = graph.get_tensor_by_name(name="Z:0")
+    cost = graph.get_tensor_by_name(name="cost:0")
+    optimizer = graph.get_operation_by_name(name="optimizer")
+
+    for i in range(10):
+        print("epoch " + str(i))
+        _, temp_cost = sess.run([optimizer, cost], feed_dict={X: x_train, Y: y_train})
+        print("Cost: " + str(temp_cost))
+
     i = 0
     for file in os.listdir(image_directory):
         filename = os.fsdecode(file)
-        image = scipy.misc.imread("out\\greyscale_200x200_labeled\\" + filename)
+        image = scipy.misc.imread("out/greyscale_200x200_labeled/" + filename)
 
         Z_arr = sess.run(Z, feed_dict={X: image.reshape((1, 200, 200, 1))})
         print(Z_arr)
@@ -35,7 +55,8 @@ with tf.Session() as sess:
         if prediction_str not in filename:
             print("OOPS<-----------------------------------------------------------------------------------------\n")
             scipy.misc.toimage(np.reshape(image, (200, 200)), cmin=0, cmax=255) \
-                .save("out\\misclassified_greyscale_200x200\\[" + str(i) + "] predicted_" + prediction_str + ", actual_" + filename + ".jpg")
+                .save("out/misclassified_greyscale_200x200/[" + str(
+                i) + "] predicted_" + prediction_str + ", actual_" + filename + ".jpg")
         else:
             print('\n')
 
--- train.py
+++ train.py
@@ -6,9 +6,11 @@ import random
 
 print(tf.__version__)
 
+export_dir = './models/test_save'
+
 learning_rate = 0.001
 regularization_coefficient = 0.01
-num_epochs = 500
+num_epochs = 1
 kernel_shapes = {"L1": [13, 13, 1, 17],
                  "L2": [11, 11, 17, 19],
                  "L3": [9, 9, 19, 21],
@@ -19,7 +21,7 @@ max_pool_shapes = {"L1": ([1, 5, 5, 1], "SAME"),
                    "L3": ([1, 3, 3, 1], "SAME"),
                    "L4": ([1, 3, 3, 1], "SAME")}
 
-x_total, y_total = ml.parse_full_data_greyscale('resources//training_mat_data//greyscale_200x200.mat')
+x_total, y_total = ml.parse_full_data_greyscale('resources/training_mat_data/greyscale_200x200.mat')
 
 (m, n_H, n_W, n_C) = x_total.shape
 
@@ -38,12 +40,14 @@ parameters = ml.initialize_parameters(kernel_shapes, regularization_coefficient)
 Z = ml.forward_propagation(X, parameters, max_pool_shapes)
 Z = tf.identity(Z, name="Z")
 cost = ml.get_cost(Z, Y)
+cost = tf.identity(cost, name="cost")
 
-optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost, name="optimizer")
 init = tf.global_variables_initializer()
 
 costs = []
 
+builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
 with tf.Session() as sess:
     sess.run(init)
     for epoch in range(num_epochs):
@@ -68,6 +72,11 @@ with tf.Session() as sess:
     test_accuracy = accuracy.eval({X: x_test, Y: y_test})
     print("Test Accuracy:", test_accuracy)
 
-    saver = tf.train.Saver()
-    saver.save(sess, '.\\models\\test_save', global_step=0)
-
+    builder.add_meta_graph_and_variables(sess,
+                                         [tf.saved_model.tag_constants.TRAINING],
+                                         signature_def_map={
+                                             "model": tf.saved_model.signature_def_utils.predict_signature_def(
+                                                 inputs={"X": X},
+                                                 outputs={"Z": Z}
+                                             )})
+builder.save()
